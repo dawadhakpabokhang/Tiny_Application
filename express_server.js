1772734717e8d6f0f9b6b8345890cc04473938da
dawadhakpabokhang <dawabokhang@gmail.com>
2017-09-14 22:44:14 +0000

    Shortening 1 Complete

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -18,11 +20,22 @@ app.get("/urls", (req, res) =>{
   res.render("urls_index", templateVars);
 });
 
+// on this path it renders the urls_new page
+app.get("/urls/new", (req, res) =>{
+  res.render("urls_new");
+});
+
 // takes user to the page displaying info based on myVar (the page is dynamic)
 app.get("/urls/:myVar", (req, res) =>{
   let templateVars = {  shortURL: req.params.myVar,
                         urlList: urlDatabase };
   res.render('urls_show', templateVars)
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 // on the /hello path respond with an html document that contains Hey!
@@ -33,3 +46,8 @@ app.get("/hello", (req, res) =>{
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// generate random number
+function generateRandomString() {
+  return Math.random().toString(36).substring(2,8);
+}
