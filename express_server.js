cb8c0d9f357afd837027985e489d6498bacad999
dawadhakpabokhang <dawabokhang@gmail.com>
2017-09-15 23:10:01 +0000

    User Specific Features Added

--- express_server.js
+++ express_server.js
@@ -8,16 +8,23 @@ app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// converted the values from strings to objects, added new userID key value pairs
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "userRandomID"
+  }
 };
 
 const users = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "yo"
   },
  "user2RandomID": {
     id: "user2RandomID",
@@ -25,6 +32,9 @@ const users = {
     password: "dishwasher-funk"
   }
 }
+
+
+// GET =========================================================================
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -40,15 +50,29 @@ app.get("/login", (req, res) => {
 
 // on the /urls path respond by rendering the urls_index page
 app.get("/urls", (req, res) =>{
-  let templateVars = { urls: urlDatabase,
-                       user: users[ req.cookies["user_id"] ] };
+  let user_id = req.cookies['user_id']
+  let resultingUrls = urlsForUser(user_id);
+  let templateVars = { urls: resultingUrls,
+                       user: users[user_id] };
   res.render("urls_index", templateVars);
 });
 
 // on this path it renders the urls_new page
 app.get("/urls/new", (req, res) =>{
-  let templateVars = { user: users[ req.cookies["user_id"] ]};
-  res.render("urls_new", templateVars);
+  if(req.cookies["user_id"]){
+    let templateVars = { user: users[ req.cookies["user_id"] ]};
+    res.render("urls_new", templateVars);
+  }else {
+    res.redirect('/login');
+  }
+});
+
+// takes user to the page displaying info based on myShortURL (the page is dynamic)
+app.get("/urls/:myShortURL", (req, res) =>{
+  let templateVars = {  shortURL: req.params.myShortURL,
+                        urlList: urlDatabase,
+                        user: users[ req.cookies["user_id"] ]};
+  res.render('urls_show', templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -56,13 +80,8 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect('/urls/' + req.params.shortURL);
 });
 
-// takes user to the page displaying info based on myVar (the page is dynamic)
-app.get("/urls/:myVar", (req, res) =>{
-  let templateVars = {  shortURL: req.params.myVar,
-                        urlList: urlDatabase,
-                        user: users[ req.cookies["user_id"] ]};
-  res.render('urls_show', templateVars)
-});
+
+// POST ====================================================================================
 
 // upon posting a new website, generate a randomstring for the website and
 // push it and its url into the UrlDatabase
@@ -84,17 +103,30 @@ app.post("/register", (req, res) => {
     let newEmail = req.body.email;
     let newPassword = req.body.password;
     res.cookie("user_id", newUserId);
-    users[newUserId] = {id: newUserId, email: newEmail, password: newPassword};
+    users[newUserId] = {id: newUserId,
+                        email: newEmail,
+                        password: newPassword
+                       };
     res.redirect('/urls');
   }
 });
 
 app.post("/urls/:id", (req, res) => {
- urlDatabase[req.params.id] = req.body.newLongUrl;
- res.redirect('/urls');
+  if(req.cookie['user_id']){
+    urlDatabase[req.params.id] = req.body.newLongUrl;
+    res.redirect('/urls');
+  }
 });
 
-// READ THROUGH THIS LOGIN
+// this route gets rid of the key and value pair given by the user and present within the urlDatabase object
+app.post("/urls/:key/delete", (req, res) => {
+  if(req.cookies['user_id']){
+    delete urlDatabase[req.params.key];
+    res.redirect('/urls');
+  }
+});
+
+// ----- LOGIN LOGIC ---------------------
 
 app.post("/login", (req, res) => {
   var searchResult = null;
@@ -102,7 +134,6 @@ app.post("/login", (req, res) => {
   //search through all users
   for(let id in users){
     let user = users[id];
-
     if(user.email === req.body.email){
       console.log("user match!" + user.email);
       searchResult = user;
@@ -116,11 +147,12 @@ app.post("/login", (req, res) => {
   else if (searchResult.password === req.body.password) {
     res.cookie('user_id', searchResult.id);
     console.log('password match');
-
     let templateVars = { urls: urlDatabase,
-                         user: searchResult };
+                         user: searchResult
+                       };
     res.render('urls_index', templateVars);
   } else {
+    //if someone is not logged in and trying to visit /urls/new have them redirected to to login page //
     console.log("Password do not match!");
     res.status(403).send();
   }
@@ -131,18 +163,31 @@ app.post("/logout", (req, res) => {
  res.redirect('/urls');
 });
 
-// this route gets rid of the key and value pair given by the user and present within the urlDatabase object
-app.post("/urls/:key/delete", (req, res) => {
-  delete urlDatabase[req.params.key];
-  res.redirect('/urls');
-});
 
-
+// Start Server =================================================================================
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
+// Custom Functions ==============================================================================
+
 // generate random number
 function generateRandomString() {
   return Math.random().toString(36).substring(2,8);
+}
+
+// takes in userId and searches the urlDatabase and returns shorturls that belong to the userId
+function urlsForUser(userId){
+  // result holds all the shorUrls matching the userId
+  let result = {};
+  for(let shortURL in urlDatabase){
+    if(urlDatabase[shortURL].userID == userId){
+      result[shortURL] = {
+                         longURl: urlDatabase[shortURL].longURL,
+                         userID: urlDatabase[shortURL].userID
+                         }
+    }
+  }
+  return result;
 }
